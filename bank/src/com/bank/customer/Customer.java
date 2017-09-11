db648d0d77e9b79d5c7ba6ab53fd1a4e832c3774
win7 <win7@win7-PC>
2017-09-11 16:26:25 +0530

    car

---
+++ bank/src/com/bank/customer/Customer.java
@@ -0,0 +1,77 @@
+package com.bank.customer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Customer {
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/mybank";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "root";
+	   
+	   public static void main(String[] args) {
+		   Connection conn = null;
+		   Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName(JDBC_DRIVER);
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		      //STEP 4: Execute a query
+		      System.out.println("Creating statement...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "SELECT * FROM customer";
+		      ResultSet rs = stmt.executeQuery(sql);
+
+		      //STEP 5: Extract data from result set
+		      while(rs.next()){
+		         //Retrieve by column name
+		         String customerNumber  = rs.getString("customerNumber");
+		         String name = rs.getString("name");
+		         String phone = rs.getString("phone");
+		         String email = rs.getString("emal");
+
+		         //Display values
+		         System.out.print("Customer Number: " + customerNumber);
+		         System.out.print(", name: " + name);
+		         System.out.print(", phone: " + phone);
+		         System.out.println(", email: " + email);
+		      }
+		      //STEP 6: Clean-up environment
+		      rs.close();
+		      stmt.close();
+		      conn.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   System.out.println("Goodbye!");
+		}//end main
+
+}
